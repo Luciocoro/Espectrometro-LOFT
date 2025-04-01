e7ee57f569de0483b2360c30f5897f0cb7466aba
Lucio Coronato <63166416+Luciocoro@users.noreply.github.com>
2025-03-31 22:04:52 -0300

    Otro update minimo

--- Kotlin Version 2/app/src/main/java/com/example/android/camera2/basic/fragments/CamarasSelectorInfo.kt
+++ Kotlin Version 2/app/src/main/java/com/example/android/camera2/basic/fragments/CamarasSelectorInfo.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.example.android.camera2.basic.R
 
-class camarasSelectorInfo: DialogFragment() {
+class CamarasSelectorInfo: DialogFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
